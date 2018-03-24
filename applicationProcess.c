c5f2b782fd86fe1ba6aa3b3430e862669acb234a
Alejo Aquili <aaquili@itba.edu.ar>
2018-03-24 13:25:45 -0300

    mq in app.

--- applicationProcess.c
+++ applicationProcess.c
@@ -6,14 +6,24 @@
 #include <errno.h>
 #include <mqueue.h>
 
-
+#define MSG_SIZE 256
+#define SLAVE_QTY 4
 
 int main(int argc, char * argv[]) 
 {
+	struct mq_attr attributes;
+	mqd_t mqDescriptor;
+	unsigned int priority;
 
-	
 	int status[SLAVE_QTY];
 	pid_t childs[SLAVE_QTY];
+
+	attributes.mq_maxmsg = argc -1;
+	attributes.mq_msgsize = MSG_SIZE;
+	attributes.mq_flags = MQ_NONBLOCK;
+
+	mqDescriptor = mq_open ("filesToHashMQ", O_WRONLY | O_NONBLOCK, 0664, &attributes);
+	
 
 	for (int i = 0; i < SLAVE_QTY; i++)
 	{ 
