320da0b184fe60a1d970a6538da79fbd7c7ff01b
Alejo Aquili <aaquili@itba.edu.ar>
2018-03-25 13:15:03 -0300

    Slave modules

--- Slave/slaveProcess.c
+++ Slave/slaveProcess.c
@@ -11,6 +11,12 @@
 #define QUEUE_NAME  "/filesToHash"
 #define MD5SUM_LENGTH 7
 
+void md5sum(size_t commandLength, char * fileToHash);
+void childProcess(int * fd, char * fileToHash);
+void parentProcess(int * fd);
+int checkQueueIsEmpty(mqd_t mqDescriptor, ssize_t bytesRead);
+void hashAFile(ssize_t bytesRead, char * fileToHash);
+
 int main(void) 
 {
 	mqd_t mqDescriptor;
@@ -19,47 +25,66 @@ int main(void)
 
 	mqDescriptor = mq_open(QUEUE_NAME, O_RDONLY | O_NONBLOCK);
 	checkFail(mqDescriptor, "mq_open Failed");
-	
+
 	while((bytesRead = mq_receive(mqDescriptor, fileToHash, MSG_SIZE, NULL)) > 0)
-	{
-		fileToHash[bytesRead] = 0;
-		pid_t child;
-		int fd[2];
-		checkFail(pipe(fd), "Pipe Failed");
+		hashAFile(bytesRead, fileToHash);
+	
+	checkQueueIsEmpty(mqDescriptor, bytesRead);
+}
 
-		child = fork();
-		checkFail(child, "Fork Failed");
-		
-		if (child == 0)
-		{
-			dup2(fd[1], 1);
-			close(fd[0]);
-			char command [MSG_SIZE + MD5SUM_LENGTH];
-			sprintf(command, "%s %s", "md5sum", fileToHash); 
-			system(command);
-			exit(1);
-		}
-		else
-		{
-			char buf[MSG_SIZE + MD5SUM_LENGTH];
-			close(fd[1]);
-	    	read(fd[0], buf, sizeof(buf));
+void md5sum(size_t commandLength, char * fileToHash)
+{
+	char command [commandLength];
+	sprintf(command, "%s %s", "md5sum", fileToHash); 
+	system(command);
+}
 
-			char new [300];
-			fflush(stdout);
-	    	sprintf(new, "%s %s%c%c", "HASH: ", buf,0,0);
-	    	printf("%s\n", new);
-		}
+void childProcess(int * fd, char * fileToHash)
+{
+	dup2(fd[1], 1);
+	close(fd[0]);
+	md5sum(MSG_SIZE + MD5SUM_LENGTH, fileToHash);
+	exit(1);
+}
 
-	}
+void parentProcess(int * fd)
+{
+	char buf[MSG_SIZE + MD5SUM_LENGTH];
+	close(fd[1]);
+	read(fd[0], buf, sizeof(buf));
+	char new [300];
+	fflush(stdout);
+	sprintf(new, "%s %s%c%c", "HASH: ", buf,0,0);
+	printf("%s\n", new);
+}
+
+
+int checkQueueIsEmpty(mqd_t mqDescriptor, ssize_t bytesRead)
+{
 	mq_close(mqDescriptor);
 	if(bytesRead < 0)
 	{
 		if(errno == EAGAIN)
 			return 0;
-		fprintf(stderr, "mq_receive Failed\n");
-		perror("mq_receive Failed");
-		exit(1);
+		fail("mq_receive Failed");
 	}	
 	return 0;
 }
+
+void hashAFile(ssize_t bytesRead, char * fileToHash)
+{
+	fileToHash[bytesRead] = 0;
+
+	pid_t child;
+	int fd[2];
+
+	checkFail(pipe(fd), "Pipe Failed");
+
+	checkFail(child = fork(), "Fork Failed");
+		
+	if (child == 0)
+		childProcess(fd, fileToHash);
+	else
+		parentProcess(fd);
+}
+
